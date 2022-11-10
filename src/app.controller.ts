882f9fdea494e1b17bdd65359936a7838179d955
Flexicon <simoncox2003@gmail.com>
2022-11-10 12:18:21 +0100

    Added 'getAllAssignmentsForCourses' endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import Assignment from './models/Assignment'
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("getAllAssignmentsForCourses")
+  getAllAssignmentsForCourses(): Assignment[]{
+    let assignments: Assignment[] = [
+      new Assignment(1,"AssignmentName1","Description1",new Date(), new Date(), new Date(), 1, 1),
+      new Assignment(2,"AssignmentName2","Description2",new Date(), new Date(), new Date(), 2, 2),
+      new Assignment(3,"AssignmentName3","Description3",new Date(), new Date(), new Date(), 3, 3),
+    ];
+
+    return assignments;
   }
 }
