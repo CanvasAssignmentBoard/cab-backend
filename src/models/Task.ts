531b56c66c647aa7d2fe0b3f62a727a99b4b03cf
flexiconn <simoncox2003@gmail.com>
2022-11-11 11:28:22 +0100

    added task model

---
+++ src/models/Task.ts
@@ -0,0 +1,32 @@
+interface ITask{
+    id : number;
+    name: string;
+    description: string;
+    created_at: Date;
+    updated_at: Date;
+    due_at: Date;
+    status: string;
+    parent_assignment_id: number;
+}
+
+class Task implements ITask{
+    id : number;
+    name: string;
+    description: string;
+    created_at: Date;
+    updated_at: Date;
+    due_at: Date;
+    status: string;
+    parent_assignment_id: number;
+
+    constructor(id: number, name: string, description: string, created_at: Date, updated_at: Date, due_at: Date, status: string, parent_assignment_id: number){
+        this.name = name;
+        this.id = id;
+        this.description = description;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.due_at = due_at;
+        this.status = status;
+        this.parent_assignment_id = parent_assignment_id;
+    }
+} export default Task;
