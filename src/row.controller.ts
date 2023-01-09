113192d40ba2d09743a0e742345e9ff2ec3eba3d
flexiconn <simoncox2003@gmail.com>
2023-01-09 22:42:26 +0100

    row endpoint

--- src/row.controller.ts
+++ src/row.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Body,
   Post,
+  Param,
 } from '@nestjs/common';
 import Assignment from './models/Assignment';
 import Task from './models/Task';
@@ -10,9 +11,20 @@ import { CreateTaskBody } from './Bodies/CreateTaskBody';
 import { randomUUID } from 'crypto';
 import DB from './Data/db';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import IRowLogic from './Logic/IRowLogic';
+import { RowLogic } from './Logic/RowLogic';
 
 @ApiTags('Task')
 @Controller('Task')
 export class RowController {
-  
+  rowLogic: IRowLogic;
+  constructor(canvasService: Canvas, dataBaseService: DB) {
+    this.rowLogic = new RowLogic(dataBaseService);
+  }
+
+  @ApiOperation({ summary :"Create assignment in course"})
+  @Post(":assignmentid/:rowid")
+  async getBoardById(@Param() params){
+    return await this.rowLogic.MoveToRow(params.assignmentid, params.rowid);
+  }
 }
